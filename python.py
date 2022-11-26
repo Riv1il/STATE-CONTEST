45ff72f9aa498812f582382fd64e34b295cb5b27
Riv1il <119123104+Riv1il@users.noreply.github.com>
2022-11-26 10:49:03 -0500

    Update python.py

---
+++ python.py
@@ -0,0 +1,25 @@
+print("choose your fighter")
+print("TYPE 1 for Ozor")
+print("TYPE 2 for Lukus")
+Player1 = int(input())
+if Player1 == 1:
+    print("Ozor")
+if Player1 == 2:
+    print("Lukus")
+if Player1 == 1:
+    print("Time for a fight")
+if Player1 == 2:
+    print("Time for a fight")
+if Player1 == 1:
+    print("Appering from your left a bandit draws a knife ready to fight")
+if Player1 == 2:
+    print("Appering from your left a bandit draws a knife ready to fight")
+if Player1 == 1:
+    attacklist = ["fireball", "earthquake", "bind","heal"]
+if Player1 == 2:
+    attacklist = ["rise shield", "slash", "sharpen sword", "triple arrow"]
+print(attacklist)
+print("TYPE 1 for fireball")
+print("TYPE 2 for earthquake")
+print("TYPE 3 for water shield")
+print("TYPE 4 for heal")
