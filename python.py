950d0bf92e8abcbaca1544ed0784729c8e25b673
Riv1il <119123104+Riv1il@users.noreply.github.com>
2022-12-04 14:58:42 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,25 +1,53 @@
-print("choose your fighter")
-print("TYPE 1 for Ozor")
-print("TYPE 2 for Lukus")
-Player1 = int(input())
-if Player1 == 1:
-    print("Ozor")
-if Player1 == 2:
-    print("Lukus")
-if Player1 == 1:
-    print("Time for a fight")
-if Player1 == 2:
-    print("Time for a fight")
-if Player1 == 1:
-    print("Appering from your left a bandit draws a knife ready to fight")
-if Player1 == 2:
-    print("Appering from your left a bandit draws a knife ready to fight")
-if Player1 == 1:
-    attacklist = ["fireball", "earthquake", "bind","heal"]
-if Player1 == 2:
-    attacklist = ["rise shield", "slash", "sharpen sword", "triple arrow"]
-print(attacklist)
-print("TYPE 1 for fireball")
-print("TYPE 2 for earthquake")
-print("TYPE 3 for water shield")
-print("TYPE 4 for heal")
+#This is the welcome screen and difficultiy select
+print("please enter a username")
+playername = input("")
+print("Please select a difficultiy")
+print("1 for Easy")
+print("2 for Normal")
+print("3 for Hard")
+leveldifficalty = int(input())
+print("Welcome to Riddle Me This")
+print("The rules are simple you are given a discription and have to spell the word that fits it.")
+#This is the easy levels
+if leveldifficalty == 1:
+    print("This celestial object orbits the sun and has a moon but isn't made of gas")
+    level1easyuseranswer = input("")
+    if level1easyuseranswer == "Earth":
+        print("Correct")
+    elif level1easyuseranswer == "earth":
+        print("Correct")
+    if level1easyuseranswer == "earth":
+        print("Correct")
+    elif level1easyuseranswer != "Earth":
+        print("Wrong")
+    print("Level 1 completed")
+    
+#This is the Normal Levels
+if leveldifficalty == 2:
+    print("This thing lives in the desert it can't move but can hurt")
+    level1Normaluseranswer = input("")
+    if level1Normaluseranswer == "Cactus":
+        print("Correct")
+    elif level1Normaluseranswer == "cactus":
+        print("Correct")
+    if level1Normaluseranswer == "cactus":
+        print("Correct")
+    elif level1Normaluseranswer != "Cactus":
+        print("Wrong")
+    print("Level 1 completed")
+    
+#This is the Hard levels
+if leveldifficalty == 3:
+    print("This invention has been used to capture history")
+    level1harduseranswer = input("")
+    if level1harduseranswer == "Camera":
+        print("Correct")
+    elif level1harduseranswer == "camera":
+        print("Correct")
+    if level1harduseranswer == "camera":
+        print("Correct")
+    elif level1harduseranswer != "Camera":
+        print("Wrong")
+    print("Level 1 completed")
+    
+#End
